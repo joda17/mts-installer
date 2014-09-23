53fcf9423de17202bec0ce62ac6a164ec988d259
joda17 <joda17@wp.pl>
2014-09-23 22:08:21 +0200

    Update patcher.sh

--- patcher.sh
+++ patcher.sh
@@ -4,4 +4,5 @@ cd /var/mt
 rm -f /var/mt/mts.sh
 wget https://raw.githubusercontent.com/joda17/mts-installer/master/mts.sh
 chmod +x /var/mt/mts.sh
+mkdir -p /var/mt/minetest
 sh /var/mt/mts.sh > /var/mt/minetest/run.log
